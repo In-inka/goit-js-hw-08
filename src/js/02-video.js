fd4891bf8f7146c858b48c1d93170330de946c92
Inna Shkilna <shkinai.1995@gmail.com>
2023-05-06 17:31:08 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+const LOCAL_STORAGE_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(playTime, 1000));
+
+saveCurrentTime(LOCAL_STORAGE_TIME);
+
+function playTime(time) {
+  const currentTime = time.seconds;
+
+  localStorage.setItem(LOCAL_STORAGE_TIME, currentTime);
+}
+function saveCurrentTime(key) {
+  const getLocalStorage = localStorage.getItem(key);
+
+  if (getLocalStorage) {
+    player.setCurrentTime(getLocalStorage);
+  }
+}
